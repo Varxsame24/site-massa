856785f793a2e171f1140b2126b12795c3be8f23
Varxsame24 <dherverman@gmail.com>
2026-02-17 02:46:03 -0300

    Add files via upload

---
+++ Site Massa/dados.js
@@ -0,0 +1,92 @@
+/* ARQUIVO DE DADOS: dados.js */
+
+const siteContent = {
+    "music": {
+        "albumCover": "imagens/BandaWallpaper.jpeg",
+        "meta": "ALBUM • 2026 • Punk/Hardcore",
+        "title": "GALINHA SEM CABEÇA",
+        "tracks": [
+            {
+                "name": "01. Intro",
+                "time": "02:45"
+            },
+            {
+                "name": "02. Painho",
+                "time": "03:12"
+            },
+            {
+                "name": "03. Ninfeta",
+                "time": "01:58"
+            }
+        ],
+        "links": {
+            "spotify": "#",
+            "apple": "#"
+        }
+    },
+    "about": {
+        "highlight": "\"A MASSA! não pede licença. Nós chegamos, tocamos alto e deixamos o palco pegando fogo.\"",
+        "text": "Formada nas ruas de Aracaju em 2025, a banda MASSA! traz a essência crua do Punk Rock combinada com a velocidade do Hardcore moderno. Somos quatro amigos unidos pelo barulho, criando músicas que falam sobre a realidade urbana com energia visceral.",
+        "members": [
+            {
+                "name": "Vocal",
+                "role": "Voz / Guitarra",
+                "img": "imagens/Ensaio2.jpeg"
+            },
+            {
+                "name": "Baixo",
+                "role": "Baixo / Backing",
+                "img": "imagens/Ensaio3.jpeg"
+            }
+        ]
+    },
+    "tour": [
+        {
+            "date": "27 FEV",
+            "venue": "Irons Pub",
+            "city": "Aracaju, SE",
+            "link": "#",
+            "status": "available"
+        }
+    ],
+    "shop": [
+        {
+            "title": "T-Shirt Black Edition",
+            "price": "R$ 57,70",
+            "img": "imagens/CamisaMassaPreta.jpg",
+            "link": "https://umapenca.com/bandamassaju/camiseta/camiseta-massa-colorida-209325.html"
+        },
+        {
+            "title": "Caneca \"Véio Pidão\"",
+            "price": "R$ 43,26",
+            "img": "imagens/CanecaMassa.jpg",
+            "link": "https://umapenca.com/bandamassaju/caneca/caneca-massa-paulinho-o-veio-pidao-222021.html"
+        },
+        {
+            "title": "Kit Bottoms Punk",
+            "price": "R$ 18,26",
+            "img": "imagens/Bottoms.jpg",
+            "link": "https://umapenca.com/bandamassaju/bottons/bottons-massa-logo-209327.html"
+        }
+    ],
+    "files": [
+        {
+            "title": "Portfólio",
+            "type": "pdf",
+            "icon": "fa-file-pdf",
+            "link": "arquivos/portfolio.pdf"
+        },
+        {
+            "title": "Rider Técnico",
+            "type": "tech",
+            "icon": "fa-sliders-h",
+            "link": "#"
+        },
+        {
+            "title": "Input List",
+            "type": "list",
+            "icon": "fa-list-ol",
+            "link": "#"
+        }
+    ]
+};
